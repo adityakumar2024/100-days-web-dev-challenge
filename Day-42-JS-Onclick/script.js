7b95c634b8216086b8a508d322441551cc5e028b
Aditya Kumar <kumaraditya29502@gmail.com>
2026-06-18 00:00:22 +0530

    Delete Day-42-JS-Onclick directory

--- Day-42-JS-Onclick/script.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "cards",
-    grabCursor: false,
-    allowTouchMove: false,
-    navigation: {
-        nextEl: "#rgt-arrow i",
-        prevEl: "#lft-arrow i",
-    },
-});
