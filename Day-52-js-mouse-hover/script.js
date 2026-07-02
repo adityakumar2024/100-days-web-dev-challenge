f2f0bf2c03c26e4ce57e4fadf2ff8fe98d9cbfdd
Aditya Kumar <kumaraditya29502@gmail.com>
2026-07-02 15:58:14 +0530

    Add HTML structure for mouse hover project

---
+++ Day-52-js-mouse-hover/script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Love Me Valentine</title>
+  <link rel="stylesheet" href="mouse.css" />
+</head>
+<body>
+
+  <h1 class="title">Hey it's Me  <span>Aditya  </span></h1>
+
+  <div class="card">
+    <img src="./71e2b1cef174820602aa8026118d7702-removebg-preview - Copy.png" alt="" />
+    <h2>Is placement possible without strong DSA fundamentals ?</h2>
+
+    <div class="btn-group">
+      <button id="yesBtn">Yes</button>
+      <button id="noBtn">No</button>
+    </div>
+  </div>
+
+  <script src="mouse.js"></script>
+</body>
+</html>
