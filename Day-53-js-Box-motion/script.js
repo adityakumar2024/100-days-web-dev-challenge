b45ea2c2423db83015cba452d828eca625333de1
Aditya Kumar <kumaraditya29502@gmail.com>
2026-07-03 00:27:55 +0530

    box motion project using java script

--- Day-53-js-Box-motion/script.js
+++ Day-53-js-Box-motion/script.js
@@ -1,28 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Box motion</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div id="main">
-        <div id="box">
-            <div id="square1" class="square"></div>
-            <div id="square2" class="square"></div>
-            <div id="square3" class="square"></div>
-        </div>
-        <div id="button">
-            <button id="button1">Play</button>
-            <button id="button2">Pause</button>
-            <button id="button3">Reverse</button>
-            <button id="button4">Restart</button>
-            <button id="button5">Timescale</button>
-        </div>
-    </div>
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  // gsap code here!
 
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.14.1/dist/gsap.min.js"></script>
-    <script src="script.js"></script>
-</body>
-</html>
+  let time = gsap. timeline({
+    paused:true
+ })
+
+time.to ("#square1",{
+    rotation:360,
+    duration: 3,
+    backgroundColor: "black"
+})
+
+time.to ("#square2",{
+    rotation:360,
+    duration: 3,
+    backgroundColor: "black"
+})
+
+time.to ("#square3",{
+    rotation:360,
+    duration: 3,
+    backgroundColor: "black"
+})
+
+time.to ("#square4",{
+    rotation:360,
+    duration: 3,
+    backgroundColor: "black"
+})
+
+time.to ("#square4",{
+    rotation:360,
+    duration: 3,
+    backgroundColor: "black"
+})
+
+let play = document.querySelector("button1")
+let pause = document.querySelector("button2")
+let reverse = document.querySelector("button3")
+let restart = document.querySelector("button4")
+let timescale = document.querySelector("button4")
+
+button1.addEventListener("click",() =>{
+  time.play()
+})
+
+button2.addEventListener("click",() =>{
+  time.pause()
+})
+
+button3.addEventListener("click",() =>{
+  time.reverse()
+})
+
+button4.addEventListener("click",() =>{
+  time.restart()
+})
+
+button5.addEventListener("click",() =>{
+  time.timeScale()
+})
+ });
+
+
+
+
+
