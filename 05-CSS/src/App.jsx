bc13e5eeb85c5a885b3306e289f3a5241189c952
Aditya Kumar <kumaraditya29502@gmail.com>
2026-07-27 20:01:20 +0530

    05_CSS Update

---
+++ 05-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Components/Button'
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
